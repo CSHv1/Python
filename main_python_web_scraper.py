31e95cc9d000f5afb0ae0e6da798f8bc939fd15a
CSHv1 <10523346+CSHv1@users.noreply.github.com>
2022-10-22 14:13:50 +0100

    Update main_python_web_scraper.py

--- main_python_web_scraper.py
+++ main_python_web_scraper.py
@@ -1,28 +1,25 @@
-# AIM: TO BUILD A WEB SCRAPER FOR A FASHION WEBSITE
-# OUTPUT TO A CSV WITH A TIMESTAMP COLUMN, THEN PLACE INTO G-DOC FOR SOME LEVEL OF COMPETITOR ANALYSIS?
-# BONUS POINTS IF YOU CAN FIGURE OUT HOW TO SCHEDULE IT
-
-# 1) Iterate through pages based on numbering system and site structure. Use this as an append.
-# 2) Can then use a while loop to create numbers for pages, then you get to an error.
-# 3) Combine all data pulls together as an appended DataFrame with the same elements
-# 4) BONUS: Can I export to a Google Sheet?
-# 5) Adding Categorisation & Gender based on page to enhance the web scraped dataset
-
-import user_agents
-import myt_category_pages
+import random
+import time
+import re
+import math
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
-import time
-import random
-import math
+import myt_category_pages
+import user_agents
+
 products_per_page = 60
 
-# user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
 # Essential in order to get past bot detection. Either this or, use something like fake agent
 # https://stackoverflow.com/questions/61400692/how-to-bypass-bot-detection-and-scrape-a-website-using-python
 
-final_df = pd.DataFrame(columns=['Day', 'Designer', 'Product Name', 'Price', 'Image', 'Link'])
+final_df = pd.DataFrame(columns=['Day',
+                                 'Designer',
+                                 'Product Name',
+                                 'Price',
+                                 'Image',
+                                 'Link',
+                                 'Category Link'])
 urls_to_scrape = []
 
 # GENERATE LISTING OF URLS BASED ON SITE STRUCTURE
@@ -35,6 +32,7 @@ for page in range(0, len(myt_category_pages.top_level_category)):
 
     # Dynamic Expression for Pages to Scrape
     product_category_amount = int((soup.find("p", class_="amount amount-has-pages")).text.replace(" products", "").strip())
+    # pages_to_scrape = 1
     pages_to_scrape = math.ceil(product_category_amount / products_per_page)
     urls_to_scrape.append(root_url)
 
@@ -97,13 +95,44 @@ for url in urls_to_scrape:
 
     df = pd.DataFrame(my_t_dict)
     df['Day'] = pd.to_datetime('now', utc=True).strftime("%Y-%m-%d")
-    df = df[['Day', 'Designer', 'Product Name', 'Price', 'Image', 'Link']]
+    df['Category Link'] = str(url)
+
+    df = df[['Day',
+             'Designer',
+             'Product Name',
+             'Price',
+             'Image',
+             'Link',
+             'Category Link']]
+
     final_df = pd.concat([final_df, df], axis=0)
     products_pulled_total = len(final_df)
-    print(f"{url} has been scraped. Total Product Count: {products_pulled_total}. Size of DF: {len(my_t_dict)}")
+    print(f"{url} has been scraped. Total Product Count: {products_pulled_total}.")
 
+# Final String Manipulation
+
+# Gender Entry Creation
+
+
+def gender_fn(url_val):
+
+    if 'men' in url_val:
+        return 'Mens'
+    else:
+        return 'Womens'
+
+
+final_df['Gender'] = [gender_fn(x) for x in final_df['Category Link']]
+
+# Hardcode currency to pounds, since we're pulling from the UK site
+final_df['Currency'] = 'GBP'
+
+# Extract top-level category from page here
+pattern = '([a-z]{1,})(.)(html)'
+final_df['Top Level Category'] = [re.search(pattern, x)[1].title() for x in final_df['Category Link']]
 
 
 # Done at the end to output to csv location
 output_timestamp = pd.to_datetime('now', utc=True).strftime("%Y-%m-%d")
 final_df.to_csv(f'{output_timestamp}_myt_product_scrape.csv', index=False, encoding='utf-8')
+
