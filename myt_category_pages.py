626105bc7cb76235995bd2627c6654329877a80b
CSHv1 <10523346+CSHv1@users.noreply.github.com>
2022-10-22 14:15:32 +0100

    Add files via upload

---
+++ myt_category_pages.py
@@ -0,0 +1,12 @@
+
+# top_level_category = ['https://www.mytheresa.com/en-gb/men/accessories.html']
+
+top_level_category = ['https://www.mytheresa.com/en-gb/clothing.html',
+                      'https://www.mytheresa.com/en-gb/bags.html',
+                      'https://www.mytheresa.com/en-gb/shoes.html',
+                      'https://www.mytheresa.com/en-gb/accessories.html',
+                      'https://www.mytheresa.com/en-gb/jewelry.html',
+                      'https://www.mytheresa.com/en-gb/men/clothing.html',
+                      'https://www.mytheresa.com/en-gb/men/bags.html',
+                      'https://www.mytheresa.com/en-gb/men/shoes.html',
+                      'https://www.mytheresa.com/en-gb/men/accessories.html']
---
+++ user_agents.py
@@ -0,0 +1,6 @@
+user_agent_list = [
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+]
